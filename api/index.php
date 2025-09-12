b38c0838e79734f816bc9d40d9254eb19e583463
95270w0 <sunyongliang233@gmail.com>
2025-09-12 09:41:21 +0800

    44444

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title class="a1">注意！</title>
-        <link rel="stylesheet" type="text/css" href="'/public/styles/styles.min.css">
+        <link rel="stylesheet" type="text/css" href="'../public/styles/styles.min.css">
 
         <script>
             function launchpopLink() {
