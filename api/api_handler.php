e9b0b617b16d056462d558dc2db19da9d92f7937
95270w0 <sunyongliang233@gmail.com>
2025-09-27 08:33:27 +0800

    7777

--- api/api_handler.php
+++ api/api_handler.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json');
 
 // $accessToken 和 $offer: 这两行定义了两个变量，用于存储访问令牌和优惠活动ID，这些将在后续的API请求中使用。
 $accessToken = '6d8300890e21e3bc737b8374031625d2';
-$offer = 146278;
+$offer = 151037;
 
 
 // $_SERVER['REQUEST_METHOD']: 检查请求方法是否为POST；如果不是，则返回错误信息并终止脚本执行。
